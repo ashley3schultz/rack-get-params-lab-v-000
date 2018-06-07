279ebfe2e3401ae21c4ded16eb58b355f16cf81c
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-07 01:53:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -19,13 +19,13 @@ class Application
       end
 
     elsif req.path.match(/add/)
-      search_term = req.params["q"] 
+      search_term = req.params["q"]
       if @@items.include?(search_term)
-        @@cart << search_term 
+        @@cart << search_term
         resp.write "added #{search_term}"
-      else 
+      else
         resp.write "We don't have that item"
-      
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)

475f685be75d86a1cb8a4ad430a294c04db10823
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-07 01:55:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
       if @@items.include?(search_term)
         @@cart << search_term
         resp.write "added #{search_term}"
-      else
+      else !@@items.include?(search_term)
         resp.write "We don't have that item"
       end
 
